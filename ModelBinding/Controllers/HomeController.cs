a5762e7fb28a43b15476e1fb4182a0699e9d13da
Orkhan Farajov <orkhankhf@code.edu.az>
2018-02-06 21:26:00 +0400

    Controllerdan View'a iki ferqli tipde data gondermeyi gosterdim. Elave olaraqda Basic crud

---
+++ ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelBinding.Models;
+using System.Dynamic;
+
+namespace ModelBinding.Controllers
+{
+	public class HomeController : Controller
+	{
+		AnimalEntities db = new AnimalEntities();
+		public ActionResult Index()
+		{
+			return View(new ModelData() { Teachers = db.Teachers.ToList(), Students = db.Students.ToList() });
+		}
+
+
+
+		[HttpPost]
+		public ActionResult Index([Bind(Include = "id,name,surname,age")] Student std)
+		{
+			std.age = Convert.ToInt32(std.age);
+			db.Students.Add(std);
+			db.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+
+
+
+		[HttpGet]
+		public ActionResult Edit(int? id)
+		{
+			var row = db.Students.Find(id);
+			return View(row);
+		}
+		[HttpPost]
+		public ActionResult Edit(int? id, Student std)
+		{
+			var row = db.Students.Find(id);
+			row.name = std.name;
+			row.surname = std.surname;
+			row.age = std.age;
+			db.SaveChanges();
+			return View(row);
+		}
+
+
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+	
+}
---
+++ ModelBinding/Models/ModelData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ModelBinding.Models;
+
+namespace ModelBinding.Models
+{
+	public class ModelData
+	{
+		public dynamic Teachers {get; set;}
+		public dynamic Students { get; set; }
+	}
+}
